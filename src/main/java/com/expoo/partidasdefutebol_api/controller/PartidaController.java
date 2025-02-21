3b8057eefc561061b89c85d30179f4df5ef4bdff
devrsmenezes <fael3008@hotmail.com>
2025-02-20 22:26:53 -0300

    WIP = work in progress

--- src/main/java/com/expoo/partidasdefutebol_api/controller/PartidaController.java
+++ src/main/java/com/expoo/partidasdefutebol_api/controller/PartidaController.java
@@ -1,4 +1,29 @@
 package com.expoo.partidasdefutebol_api.controller;
 
+import com.expoo.partidasdefutebol_api.dto.PartidaDTO;
+import com.expoo.partidasdefutebol_api.model.Partida;
+import com.expoo.partidasdefutebol_api.service.PartidaService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/partidas")
 public class PartidaController {
+    @Autowired
+    private PartidaService partidaService;
+
+    @PostMapping
+    public ResponseEntity<Partida> cadastrarPartida(@Valid @RequestBody PartidaDTO partidaDTO) {
+        Partida novaPartida = partidaService.cadastrarPartida(partidaDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novaPartida);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Partida> atualizarPartida(@PathVariable Long id, @Valid @RequestBody PartidaDTO partidaDTO) {
+        Partida partidaAtualizada = partidaService.atualizarPartida(id, partidaDTO);
+        return ResponseEntity.ok(partidaAtualizada);
+    }
 }
